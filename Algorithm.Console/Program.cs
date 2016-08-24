1c10b663c85fb8f855d8bdd9cadb3bb1fa7df9b4
Peter <Peter@PETER>
2016-08-24 22:47:18 +0200

    Add project files.

---
+++ Algorithm.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms;
+
+namespace AlgorithmConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new List<int>() { 555, 21, 3, 4 }; //Możesz też zrobić wczytywanie z konsoli
+
+            //var maxDrawDownAlgorithm = new MaxDrawDownAlgorithm();
+
+            var result = MaxDrawDownAlgorithm.Solve(data);
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
