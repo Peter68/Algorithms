8279a5d3b6e94b85b222e6bec082087511cf7acd
Piotr ZiÄ?ba <peterek68@live.com>
2016-08-27 17:32:17 +0200

    Add project files.

---
+++ Algorithms/MaxDrawDownAlgorithms.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class MaxDrawDownAlgorithm
+    {
+        public static int Solve(List<int> lista)
+        {
+            int spadek = 0;
+            for (int i = 0; i < lista.Count; i++)
+            {
+                for (int j = i + 1; j < lista.Count; j++)
+                {
+                    if (lista[i] > lista[j])
+                    {
+
+                        if ((lista[i] - lista[j]) > spadek)
+                        {
+                            spadek = lista[i] - lista[j];
+                        }
+                    }
+                }
+
+            }
+            return spadek;
+        }
+    }
+}
